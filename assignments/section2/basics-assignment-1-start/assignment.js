5de2d5bdf133ae1e7576ef9ee402f3b3b3b8419f
Mohamed Shalaby <mhdshalabystar@gmail.com>
2023-01-20 01:07:11 +0300

    Done from the first section2 Assignment

---
+++ assignments/section2/basics-assignment-1-start/assignment.js
@@ -0,0 +1,11 @@
+const userInput = 10;
+let result = 18 + userInput;
+
+result = result - 5;
+result = result / 2;
+result = result * 2;
+
+// The Value of user Input will not be chaged
+
+alert(userInput);
+alert(result);
