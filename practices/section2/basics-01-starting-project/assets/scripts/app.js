6ca72db6e275df18d5494780414aac7141fd8c88
Mohamed Shalaby <mhdshalabystar@gmail.com>
2023-01-20 01:32:17 +0300

    Video 28- Add a custom function

--- practices/section2/basics-01-starting-project/assets/scripts/app.js
+++ practices/section2/basics-01-starting-project/assets/scripts/app.js
@@ -1,11 +1,20 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function add(num1, num2) {
+  const result = num1 + num2;
+  alert(result);
+}
+
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
 // Try linebreak
 // let errorMessage = "An error \n occurred!";
+
+// Call Add Function
+add(5, 5);
+add(50, 50);
 
 outputResult(currentResult, errorMessage);
