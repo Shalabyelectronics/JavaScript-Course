33402dd49c83804a369c88cb03cbc1681828c706
Mohamed Shalaby <mhdshalabystar@gmail.com>
2023-01-20 02:35:24 +0300

    video 34- Executing Function Indirectly

--- practices/section2/basics-01-starting-project/assets/scripts/app.js
+++ practices/section2/basics-01-starting-project/assets/scripts/app.js
@@ -3,22 +3,24 @@ let currentResult = defaultResult;
 
 // You can define your function from any where on your script but it is a good practice to keep them on the top.
 function add(num1, num2) {
-  const result = num1 + num2;
+  const result = currentResult + userInput.value;
+  let calculationDescription = `${currentResult} + ${userInput.value}`;
+  outputResult(result, calculationDescription);
   // Using an alert inside a function is not a good practice
   // alert(result);
   // But is a good practice to add return keyword instead to return the value
-  return result;
+  // return result;
 }
 
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
-
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+// currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
 // Try linebreak
 // let errorMessage = "An error \n occurred!";
 
 // Call Add Function
-currentResult = add(5, 5);
-add(50, 50);
+// currentResult = add(5, 5);
+// add(50, 50);
 
-outputResult(currentResult, calculationDescription);
+// Add event listiner t add button
+
+addBtn.addEventListener("click", add);
