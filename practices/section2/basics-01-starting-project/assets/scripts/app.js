9b8a85add74ab4220a331eb2505ac9d14575b3aa
Mohamed Shalaby <mhdshalabystar@gmail.com>
2023-01-20 01:43:35 +0300

    Video 29- returning Values

--- practices/section2/basics-01-starting-project/assets/scripts/app.js
+++ practices/section2/basics-01-starting-project/assets/scripts/app.js
@@ -3,7 +3,10 @@ let currentResult = defaultResult;
 
 function add(num1, num2) {
   const result = num1 + num2;
-  alert(result);
+  // Using an alert inside a function is not a good practice
+  // alert(result);
+  // But is a good practice to add return keyword instead to return the value
+  return result;
 }
 
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
@@ -14,7 +17,7 @@ let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 // let errorMessage = "An error \n occurred!";
 
 // Call Add Function
-add(5, 5);
+currentResult = add(5, 5);
 add(50, 50);
 
-outputResult(currentResult, errorMessage);
+outputResult(currentResult, calculationDescription);
