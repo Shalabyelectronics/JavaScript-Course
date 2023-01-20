444f876d18d82504d1545007485dd0ceda6e48bc
Mohamed Shalaby <mhdshalabystar@gmail.com>
2023-01-20 04:03:25 +0300

    Video 38 - Splitting Code into function

--- practices/section2/basics-01-starting-project/assets/scripts/app.js
+++ practices/section2/basics-01-starting-project/assets/scripts/app.js
@@ -2,14 +2,25 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 // You can define your function from any where on your script but it is a good practice to keep them on the top.
+
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
 function add() {
   // ----------------------------------------
   // To Check the type of data stored in variable
   // console.log(typeof userInput.value);
   // ----------------------------------------
 
-  const result = currentResult + parseInt(userInput.value);
-  let calculationDescription = `${currentResult} + ${userInput.value}`;
+  // ----------------------------------------
+  // Don't repeat the same code everywhere and it is better to define the code the repetitive used
+  // ----------------------------------------
+  const enteredNumber = getUserNumberInput();
+  // so You need just to edit  getUserNumberInput function and it will change every where
+
+  const result = currentResult + enteredNumber;
+  let calculationDescription = `${currentResult} + ${enteredNumber}`;
   outputResult(result, calculationDescription);
   currentResult = result;
   // Using an alert inside a function is not a good practice
