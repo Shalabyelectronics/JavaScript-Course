b959644fe1667f0d345085a68d47bd1e505f2a85
Mohamed Shalaby <mhdshalabystar@gmail.com>
2023-01-22 04:57:16 +0300

    Video 83- monster killer - add if statement to check the win condition

--- practices/section4/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ practices/section4/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,9 +1,19 @@
-const ATTACK_AMOUNT = 10;
+const ATTACK_PLAYER_AMOUNT = 10;
+const ATTACK_MONSTER_AMOUNT = 14;
 let playerHealth = 100;
-let currentHealt = playerHealth;
-let monsterHealth = playerHealth;
+let currentPlayerHealth = playerHealth;
+let currentMonsterHealth = playerHealth;
 
 function attackHandler() {
-  dealMonsterDamage(ATTACK_AMOUNT);
+  currentPlayerHealth -= dealPlayerDamage(ATTACK_MONSTER_AMOUNT);
+  currentMonsterHealth -= dealMonsterDamage(ATTACK_PLAYER_AMOUNT);
+
+  if (currentPlayerHealth > 0 && currentMonsterHealth <= 0) {
+    alert("Player WON!");
+  } else if (currentMonsterHealth > 0 && currentPlayerHealth <= 0) {
+    alert("Monster WON!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert("There is a Draw Fight Again!");
+  }
 }
 attackBtn.addEventListener("click", attackHandler);
