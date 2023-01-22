7a00c19966685d517fd259db492e17ab845ba9b6
Mohamed Shalaby <mhdshalabystar@gmail.com>
2023-01-22 05:18:23 +0300

    Video 83- monster killer - add Strong attack hundler with attack amount

--- practices/section4/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ practices/section4/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,12 +1,21 @@
 const ATTACK_PLAYER_AMOUNT = 10;
 const ATTACK_MONSTER_AMOUNT = 14;
+const STRONG_ATTACK_AMOUNT = 17;
 let playerHealth = 100;
 let currentPlayerHealth = playerHealth;
 let currentMonsterHealth = playerHealth;
 
-function attackHandler() {
+function attackAmount(mode) {
+  let damageAmount;
+  if (mode === "ATTACK") {
+    damageAmount = ATTACK_PLAYER_AMOUNT;
+  } else if (mode === "STRONG_ATTACK") {
+    damageAmount = STRONG_ATTACK_AMOUNT;
+  } else {
+    return;
+  }
   currentPlayerHealth -= dealPlayerDamage(ATTACK_MONSTER_AMOUNT);
-  currentMonsterHealth -= dealMonsterDamage(ATTACK_PLAYER_AMOUNT);
+  currentMonsterHealth -= dealMonsterDamage(damageAmount);
 
   if (currentPlayerHealth > 0 && currentMonsterHealth <= 0) {
     alert("Player WON!");
@@ -16,4 +25,13 @@ function attackHandler() {
     alert("There is a Draw Fight Again!");
   }
 }
+
+function attackHandler() {
+  attackAmount("ATTACK");
+}
+
+function strongAttackHandler() {
+  attackAmount("STRONG_ATTACK");
+}
 attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
