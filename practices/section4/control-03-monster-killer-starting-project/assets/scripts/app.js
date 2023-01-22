2e2cc80051c5d6be475c55f48674cf0c0105afa6
Mohamed Shalaby <mhdshalabystar@gmail.com>
2023-01-22 09:53:56 +0300

    Video 87- monster killer - add heal player functionality

--- practices/section4/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ practices/section4/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,9 +1,23 @@
 const ATTACK_PLAYER_AMOUNT = 10;
 const ATTACK_MONSTER_AMOUNT = 14;
 const STRONG_ATTACK_AMOUNT = 17;
-let playerHealth = 100;
-let currentPlayerHealth = playerHealth;
-let currentMonsterHealth = playerHealth;
+const HEAL_AMOUNT = 20;
+const chosenMaxLife = 100;
+let currentPlayerHealth = chosenMaxLife;
+let currentMonsterHealth = chosenMaxLife;
+
+adjustHealthBars(chosenMaxLife);
+
+function roundEnd() {
+  currentPlayerHealth -= dealPlayerDamage(ATTACK_MONSTER_AMOUNT);
+  if (currentPlayerHealth > 0 && currentMonsterHealth <= 0) {
+    alert("Player WON!");
+  } else if (currentMonsterHealth > 0 && currentPlayerHealth <= 0) {
+    alert("Monster WON!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert("There is a Draw Fight Again!");
+  }
+}
 
 function attackAmount(mode) {
   let damageAmount;
@@ -14,16 +28,8 @@ function attackAmount(mode) {
   } else {
     return;
   }
-  currentPlayerHealth -= dealPlayerDamage(ATTACK_MONSTER_AMOUNT);
   currentMonsterHealth -= dealMonsterDamage(damageAmount);
-
-  if (currentPlayerHealth > 0 && currentMonsterHealth <= 0) {
-    alert("Player WON!");
-  } else if (currentMonsterHealth > 0 && currentPlayerHealth <= 0) {
-    alert("Monster WON!");
-  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
-    alert("There is a Draw Fight Again!");
-  }
+  roundEnd();
 }
 
 function attackHandler() {
@@ -33,5 +39,23 @@ function attackHandler() {
 function strongAttackHandler() {
   attackAmount("STRONG_ATTACK");
 }
+
+function healHandler() {
+  let totalAmountHeal;
+
+  if (currentPlayerHealth >= chosenMaxLife - HEAL_AMOUNT) {
+    alert("You can't Heal now!!");
+    totalAmountHeal = chosenMaxLife - currentPlayerHealth;
+  } else {
+    totalAmountHeal = HEAL_AMOUNT;
+  }
+  currentPlayerHealth += totalAmountHeal;
+  increasePlayerHealth(totalAmountHeal);
+  roundEnd();
+  // console.log("current player health:", currentPlayerHealth);
+  // console.log("Stutus progress value", playerHealthBar.value);
+}
+
 attackBtn.addEventListener("click", attackHandler);
 strongAttackBtn.addEventListener("click", strongAttackHandler);
+healBtn.addEventListener("click", healHandler);
