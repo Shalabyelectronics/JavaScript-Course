bc73e1168f4a1f7f0a708dc8eed7f29981debeb5
Mohamed Shalaby <mhdshalabystar@gmail.com>
2023-01-28 00:01:43 +0300

    V126: Rock sessor Paper game : add event listener

--- practices/section6/rock-paper-sesor-game/app.js
+++ practices/section6/rock-paper-sesor-game/app.js
@@ -1,1 +1,27 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById("start-game-btn");
+// Because of Hoisting feature we can call function befor its declared
+// startGame();
+
+function startGame() {
+  console.log("Game starting .....");
+}
+
+startGameBtn.addEventListener("click", startGame);
+
+/*
+
+
+// Method is a function withen an Object as below
+
+const yourName = {
+  ownerName: "Shalaby",
+  greatMe: function greatMe(userName) {
+    userName = true ? userName : "buddy";
+    console.log(`Hi ${userName} I'm ${this.ownerName} `);
+  },
+};
+// greatme is a method 
+yourName.greatMe("samer");
+
+
+*/
