7cf2b733aca3846e9107ca3e4641e64042581339
Mohamed Shalaby <mhdshalabystar@gmail.com>
2023-01-28 01:54:18 +0300

    V129: Try to add logic to game paper ,rock, sessors

--- practices/section6/rock-paper-sesor-game/app.js
+++ practices/section6/rock-paper-sesor-game/app.js
@@ -2,8 +2,27 @@ const startGameBtn = document.getElementById("start-game-btn");
 // Because of Hoisting feature we can call function befor its declared
 // startGame();
 
+function getUserInput() {
+  const availableChoices = {
+    1: "rock",
+    2: "paper",
+    3: "scissors",
+    4: "Random",
+  };
+  const userChoose = prompt(
+    "Please Choose from 1 to 4 Only\n(1) for paper,\n(2) for rock,\n(3) for scissors or\n(4) for random."
+  );
+  const result = availableChoices[userChoose] ? true : false;
+  if (result) {
+    console.log(`Your choice is ${availableChoices[userChoose]}`);
+  } else {
+    console.log("Pleae Choose Wisly");
+  }
+}
+
 function startGame() {
   console.log("Game starting .....");
+  getUserInput();
 }
 
 startGameBtn.addEventListener("click", startGame);
