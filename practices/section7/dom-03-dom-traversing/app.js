85b4919258b77b0959749227ba39be017380105e
Mohamed Shalaby <mhdshalabystar@gmail.com>
2023-02-01 02:50:43 +0300

    Video 158: Pactice - Selecting Sibling Elements.

---
+++ practices/section7/dom-03-dom-traversing/app.js
@@ -0,0 +1,5 @@
+const ul = document.body.firstElementChild.nextElementSibling;
+const firstLi = ul.firstElementChild;
+const header = ul.previousElementSibling;
+console.log(firstLi);
+console.log(header);
