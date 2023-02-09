c136c0f0180d2d89663963d853ad681796358d1b
Mohamed Shalaby <mhdshalabystar@gmail.com>
2023-02-09 18:49:44 +0300

    Section 9 - Object deleteing properties

---
+++ practices/section9/objects-03-deleting-properties/assets/scripts/objects.js
@@ -0,0 +1,20 @@
+let person = {
+  name: "Max",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+  greet: function () {
+    alert("Hi there!");
+  },
+};
+
+// ...
+
+// person.age = 31;
+delete person.age;
+delete person.greet;
+
+// person.age = undefined;
+// person.age = null;
+person.isAdmin = true;
+
+console.log(person);
