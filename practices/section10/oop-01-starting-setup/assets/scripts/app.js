a4466b750b3caca32cf13260a1b6ca02422ad163
Mohamed Shalaby <mhdshalabystar@gmail.com>
2023-02-12 13:27:03 +0300

    Section 10: OOP-2 Working with Constructor Methods

--- practices/section10/oop-01-starting-setup/assets/scripts/app.js
+++ practices/section10/oop-01-starting-setup/assets/scripts/app.js
@@ -1,19 +1,32 @@
+class Product {
+  constructor(
+    title = "default",
+    imageUrl = "https://quickbooks.intuit.com/oidam/intuit/sbseg/en_row/quickbooks/web/content/default-placeholder.png",
+    price = 0,
+    description = "No Description."
+  ) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.price = price;
+    this.description = description;
+  }
+}
+
 const productList = {
   myProduct: [
-    {
-      title: "Samsung smart tv",
-      imageUrl:
-        "https://images.samsung.com/is/image/samsung/p5/sa/tvs/smart-tv/highlights/smart-tv-f02-pc001.jpg?$ORIGIN_JPG$",
-      price: "500",
-      description: "A nice tv to have",
-    },
-    {
-      title: "Platstation 5",
-      imageUrl:
-        "https://www.alhub.me/wp-content/uploads/2022/09/ps5-1-1591910417-1.png",
-      price: "300",
-      description: "Start to live an Adventure",
-    },
+    new Product(
+      "Samsung smart tv",
+      "https://images.samsung.com/is/image/samsung/p5/sa/tvs/smart-tv/highlights/smart-tv-f02-pc001.jpg?$ORIGIN_JPG$",
+      "500",
+      "A nice tv to have"
+    ),
+    new Product(
+      "Platstation 5",
+      "https://www.alhub.me/wp-content/uploads/2022/09/ps5-1-1591910417-1.png",
+      "300",
+      "Start to live an Adventure"
+    ),
+    new Product(),
   ],
   render() {
     const productBoard = document.getElementById("app");
