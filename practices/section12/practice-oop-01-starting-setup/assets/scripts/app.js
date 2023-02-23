e8297c6b4ad932b17331c58e9fdcbdaefc5ca87c
Mohamed Shalaby <mhdshalabystar@gmail.com>
2023-02-23 18:02:58 +0300

    swap cards between to box my try

---
+++ practices/section12/practice-oop-01-starting-setup/assets/scripts/app.js
@@ -0,0 +1,58 @@
+const finishBtn = document.querySelector("p1");
+const btn = document.querySelectorAll("button");
+
+const finishedProject = (e) => {
+  e.target.removeEventListener("click", finishedProject);
+  const copyNode = e.target.parentElement.cloneNode(true);
+  e.target.parentElement.remove();
+  copyNode.addEventListener("click", activeProject);
+  document.getElementById("active-projects").lastElementChild.append(copyNode);
+};
+
+const activeProject = (e) => {
+  e.target.removeEventListener("click", activeProject);
+  const copyNode = e.target.parentElement.cloneNode(true);
+  e.target.parentElement.remove();
+  copyNode.addEventListener("click", finishedProject);
+  document
+    .getElementById("finished-projects")
+    .lastElementChild.append(copyNode);
+};
+
+const swapCards = (nodeElement) => {
+  // console.log(nodeElement.target.parentElement.parentElement.parentElement);
+  nodeElement.target.removeEventListener("click", swapCards);
+  const jobSection =
+    nodeElement.target.parentElement.parentElement.parentElement.id;
+  const copyNode = nodeElement.target.parentElement.cloneNode(true);
+  nodeElement.target.parentElement.remove();
+  if (jobSection === "active-projects") {
+    // console.log(copyNode);
+    copyNode.lastElementChild.innerText = "Activate";
+    copyNode.lastElementChild.addEventListener("click", swapCards);
+    document
+      .getElementById("finished-projects")
+      .lastElementChild.append(copyNode);
+  } else if (jobSection === "finished-projects") {
+    console.log(copyNode.lastElementChild);
+    copyNode.lastElementChild.innerText = "Finish";
+    copyNode.lastElementChild.addEventListener("click", swapCards);
+    document
+      .getElementById("active-projects")
+      .lastElementChild.append(copyNode);
+  }
+};
+
+// Filter only action Buttons
+
+const actionBtn = Array.from(btn).filter((b) => !b.getAttribute("class"));
+
+actionBtn.forEach((b) => b.addEventListener("click", swapCards));
+
+/*
+Event Object 
+
+e.target.innerText
+e.target.parentElement
+
+*/
